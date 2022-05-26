e23b2a739831429144d0ede9455341a90396cd05
student <student@C320U01>
2022-05-26 13:59:50 +0300

    Добавьте файлы проекта.

---
+++ OS_practice_6/OS_practice_6.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <conio.h>
+#include <Windows.h>
+#include <WinUser.h>
+
+char mem[512];
+
+void draw() {
+    for (short i = 0; i < 512; i++) {
+        COORD pos;
+        pos.X = i % 32 + 1;
+        pos.Y = i / 32 + 2;
+        SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+        std::cout << mem[i];
+    }
+}
+
+int main() {
+    CONSOLE_CURSOR_INFO inf;
+    GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &inf);
+    inf.bVisible = false;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &inf);
+    std::cout << "3\n";
+    std::cout << char(218);
+    for (short i = 0; i < 32; i++) std::cout << char(196);
+    std::cout << char(191) << '\n';
+    for (short i = 0; i < 16; i++) {
+        std::cout << char(179);
+        for (short j = 0; j < 32; j++) std::cout << ' ';
+        std::cout << char(179) << '\n';
+    }
+    std::cout << char(192);
+    for (short i = 0; i < 32; i++) std::cout << char(196);
+    std::cout << char(217) << '\n';
+    for (short i = 0; i < 512; i++) mem[i] = ' ';
+    for (;;) {
+        char input = _getch();
+        if (input == 'a') {
+            int size;
+            std::cin >> size;
+            for (short i = 0; i < 512; i++) {
+                bool ok = true;
+                for (short j = 0; j < size; j++) {
+                    if (mem[i + j] != ' ') {
+                        ok = false;
+                        break;
+                    }
+                }
+                if (ok) {
+                    for (short j = 0; j < size; j++) {
+                        mem[i + j] = 'a';
+                        
+                    }
+                    break;
+                }
+            }
+            draw();
+        } else if (input == 'd') {
+            input = _getch();
+            for (short i = 0; i < 512; i++) if (mem[i] == input) mem[i] = ' ';
+            draw();
+        }
+    }
+}
