2b5724e3e2796c02770b4e0553ff6bca95b4df22
Kostya Bespalov <96658739+PredsedateI@users.noreply.github.com>
2022-05-31 04:56:01 +0300

    Update OS_practice_6.cpp
    v2.0

--- OS_practice_6/OS_practice_6.cpp
+++ OS_practice_6/OS_practice_6.cpp
@@ -3,16 +3,26 @@
 #include <Windows.h>
 #include <WinUser.h>
 
-char mem[512];
+char mem[640];
+
+void clear() {
+    COORD pos;
+    pos.X = 0;
+    pos.Y = 20;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    std::cout << "                              \n                              ";
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
 
 void draw() {
-    for (short i = 0; i < 512; i++) {
-        COORD pos;
-        pos.X = i % 32 + 1;
-        pos.Y = i / 32 + 2;
+    COORD pos;
+    for (short i = 0; i < 640; i++) {
+        pos.X = i % 40 + 1;
+        pos.Y = i / 40 + 2;
         SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
         std::cout << mem[i];
     }
+    clear();
 }
 
 int main() {
@@ -20,63 +30,68 @@ int main() {
     GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &inf);
     inf.bVisible = false;
     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &inf);
-    std::cout << "3\n";
+    std::cout << "3. Variable Sections\n";
     std::cout << char(218);
-    for (short i = 0; i < 32; i++) std::cout << char(196);
+    for (short i = 0; i < 40; i++) std::cout << char(196);
     std::cout << char(191) << '\n';
     for (short i = 0; i < 16; i++) {
         std::cout << char(179);
-        for (short j = 0; j < 32; j++) std::cout << ' ';
+        for (short j = 0; j < 40; j++) std::cout << ' ';
         std::cout << char(179) << '\n';
     }
     std::cout << char(192);
-    for (short i = 0; i < 32; i++) std::cout << char(196);
-    std::cout << char(217) << '\n';
-    for (short i = 0; i < 512; i++) mem[i] = ' ';
-    char alphabet[26];
+    for (short i = 0; i < 40; i++) std::cout << char(196);
+    std::cout << char(217) << "\na - add task   d - delete task\n";
+    for (short i = 0; i < 640; i++) mem[i] = ' ';
+    bool taskLetterIsFree[26];
+    for (short i = 0; i < 26; i++) taskLetterIsFree[i] = true;
     for (;;) {
         char input = _getch();
+        clear();
         if (input == 'a') {
-            int size;
-            std::cin >> size;
-            char letter = 96;
-            for (short i = 1; i <= 26; i++) {
-                bool letterok = true;
-                for (short j = 0; j < 26; j++) {
-                    if (alphabet[j] == letter + i) {
-                        letterok = false;
-                        break;
-                    }
-                }
-                if (letterok) {
-                    letter = 96 + i;
+            char taskLetter = ' ';
+            for (short i = 0; i < 26; i++) {
+                if (taskLetterIsFree[i]) {
+                    taskLetter = 97 + i;
+                    taskLetterIsFree[i] = false;
                     break;
                 }
             }
-            if (letter == 96) {
-                std::cout << "error\n";
+            if (taskLetter == ' ') {
+                std::cout << "err: no free task letters";
                 continue;
             }
-            for (short i = 0; i < 512; i++) {
-                bool ok = true;
-                for (short j = 0; j < size; j++) {
-                    if (mem[i + j] != ' ') {
-                        ok = false;
+            std::cout << "Size of new task: ";
+            int size;
+            std::cin >> size;
+            if (size > 640) {
+                std::cout << "err: not enough free space";
+                taskLetterIsFree[taskLetter - 97] = true;
+            } else {
+                for (short i = 0; i < 640 - (size - 1); i++) {
+                    bool ok = true;
+                    for (short j = 0; j < size; j++) {
+                        if (mem[i + j] != ' ') {
+                            ok = false;
+                            break;
+                        }
+                    }
+                    if (ok) {
+                        for (short j = 0; j < size; j++) mem[i + j] = taskLetter;
+                        draw();
                         break;
                     }
-                }
-                if (ok) {
-                    for (short j = 0; j < size; j++) {
-                        mem[i + j] = letter;
-                        
+                    if (i == 640 - size - 1 and ok == false) {
+                        std::cout << "err: not enough free space";
+                        taskLetterIsFree[taskLetter - 97] = true;
                     }
-                    break;
                 }
             }
-            draw();
         } else if (input == 'd') {
-            input = _getch();
-            for (short i = 0; i < 512; i++) if (mem[i] == input) mem[i] = ' ';
+            std::cout << "Letter of task: ";
+            std::cin >> input;
+            for (short i = 0; i < 640; i++) if (mem[i] == input) mem[i] = ' ';
+            taskLetterIsFree[input - 97] = true;
             draw();
         }
     }
